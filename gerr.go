9f96da07a5609ee2086f975aad4111adf6ceb6f2
GuoFlight <1754811791@qq.com>
2024-11-01 01:16:35 +0800

    新增链式构建Err的能力，修复嵌套函数调用时获取到的caller信息不准的问题

--- gerr.go
+++ gerr.go
@@ -6,44 +6,77 @@ import (
 	"runtime"
 )
 
-func NewErr(text string, traceID ...string) *Gerr {
-	// 获取TraceID
-	traceIDTemp := ""
-	if len(traceID) > 0 {
-		traceIDTemp = traceID[0]
-	} else {
-		traceIDTemp = uuid.NewV4().String()
-	}
+var KeyNameTraceId = "traceId"
 
-	// 获取调用者信息
-	errFuncName := ""
-	pc, errFile, errLine, ok := runtime.Caller(1)
-	if ok {
-		errFuncName = runtime.FuncForPC(pc).Name()
-	}
-
-	// return
-	return &Gerr{
-		s:       text,
-		TraceID: traceIDTemp,
-		ErrFile: errFile,
-		ErrFunc: errFuncName,
-		ErrLine: errLine,
-	}
+type Gerr struct {
+	s       string // 保存错误信息
+	TraceID string
+	ErrFile string
+	ErrFunc string
+	ErrLine int
 }
 
-// NewErrWithCtx 从ctx中获取traceId，创建Gerr对象
-func NewErrWithCtx(ctx context.Context, text string) *Gerr {
-	traceId, ok := ctx.Value("traceId").(string)
+type Entry struct {
+	Skip    int
+	TraceID string
+}
+
+// Error 兼容原生error
+func (g Gerr) Error() string {
+	return g.s
+}
+
+// SetSkip 设置caller的skip值
+func SetSkip(skip int) *Entry {
+	var entry Entry
+	return entry.SetSkip(skip)
+}
+func (e *Entry) SetSkip(skip int) *Entry {
+	e.Skip = skip
+	return e
+}
+
+// SetTraceId 设置TraceId
+func SetTraceId(traceId string) *Entry {
+	var entry Entry
+	return entry.SetTraceId(traceId)
+}
+func (e *Entry) SetTraceId(traceId string) *Entry {
+	e.TraceID = traceId
+	return e
+}
+
+// SetTraceIdByCtx 从context中获取并设置traceId
+func SetTraceIdByCtx(ctx context.Context) *Entry {
+	var entry Entry
+	return entry.SetTraceIdByCtx(ctx)
+}
+func (e *Entry) SetTraceIdByCtx(ctx context.Context) *Entry {
+	traceId, ok := ctx.Value(KeyNameTraceId).(string)
 	if !ok {
 		traceId = uuid.NewV4().String()
 	}
+	e.TraceID = traceId
+	return e
+}
 
+// NewErr 新建Err
+func (e *Entry) NewErr(text string) *Gerr {
 	// 获取调用者信息
+	skip := e.Skip
+	if skip == 0 {
+		skip = 1
+	}
 	errFuncName := ""
-	pc, errFile, errLine, ok := runtime.Caller(1)
+	pc, errFile, errLine, ok := runtime.Caller(skip)
 	if ok {
 		errFuncName = runtime.FuncForPC(pc).Name()
+	}
+
+	// 获取traceId
+	traceId := e.TraceID
+	if traceId == "" {
+		traceId = uuid.NewV4().String()
 	}
 
 	// return
@@ -55,16 +88,8 @@ func NewErrWithCtx(ctx context.Context, text string) *Gerr {
 		ErrLine: errLine,
 	}
 }
-
-type Gerr struct {
-	s       string // 保存错误信息
-	TraceID string
-	ErrFile string
-	ErrFunc string
-	ErrLine int
-}
-
-// 兼容原生error
-func (g Gerr) Error() string {
-	return g.s
+func NewErr(text string) *Gerr {
+	traceID := uuid.NewV4().String()
+	var entry Entry
+	return entry.SetSkip(2).SetTraceId(traceID).NewErr(text)
 }
--- gerr_test.go
+++ gerr_test.go
@@ -3,11 +3,24 @@ package gerror
 import (
 	"context"
 	"fmt"
+	"strings"
 	"testing"
 )
 
-func TestNewErrWithCtx(t *testing.T) {
-	ctx := context.WithValue(context.Background(), "traceId", "abcdefghijklmn")
-	gerr := NewErrWithCtx(ctx, "error")
+func MyTest1(text string) {
+	fmt.Println(strings.Repeat("-----------------------", 3))
+	// gerr := NewErr(text)
+	// gerr := SetSkip(2).NewErr(text)
+	// gerr := SetTraceId("IamTraceId").SetSkip(2).NewErr(text)
+	// gerr := SetSkip(2).SetTraceId("IamTraceId").NewErr(text)
+	gerr := SetSkip(2).SetTraceIdByCtx(context.WithValue(context.Background(), KeyNameTraceId, "IamTraceId")).NewErr(text)
+	fmt.Println(gerr.ErrFile)
+	fmt.Println(gerr.ErrLine)
+	fmt.Println(gerr.ErrFunc)
 	fmt.Println(gerr.TraceID)
+	fmt.Println(gerr.Error())
+	fmt.Println(strings.Repeat("-----------------------", 3))
+}
+func TestGerr(t *testing.T) {
+	MyTest1("test")
 }
